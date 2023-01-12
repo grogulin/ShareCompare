09f15f8fea7ec2283b62261b86ab014b8e4f94a1
Yaroslav Grogul <time7bass@gmail.com>
2023-01-12 19:34:45 +0100

    Initial Commit

---
+++ ShareCompare/ShareCompareApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareCompareApp.swift
+//  ShareCompare
+//
+//  Created by Ярослав Грогуль on 12.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShareCompareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
